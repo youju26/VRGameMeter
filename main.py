2dd7b6539d7dbf4ed4614f374ec0ea959cfe81e6
Julius Friesen <julius.friesen.26@outlook.com>
2024-09-19 22:24:16 +0200

    Code optimizations

--- main.py
+++ main.py
@@ -27,63 +27,55 @@ def get_name(appid):
     else:
         return None
     
-def list_games():
-    connection = connect_to_db()
-    cursor = connection.cursor()
+def list_games(cursor):
     cursor.execute("SELECT name, steam_reviews from vr_games order by steam_reviews desc")
     games = cursor.fetchall()
     for game in games:
         print(f"Name: {game[0]}, Steam_Reviews: {game[1]}")
-    cursor.close()
-    connection.close()
 
-def add_game(steam_id, vr_only):
-    connection = connect_to_db()
-    cursor = connection.cursor()
+def add_game(cursor, connection, steam_id, vr_only):
     steam_reviews = get_reviews(steam_id)
     name = get_name(steam_id)
     cursor.execute(f"INSERT INTO vr_games (name, steam_id, steam_reviews, vr_only) VALUES (\"{name}\", {steam_id}, {steam_reviews}, {vr_only}) on duplicate key update steam_reviews = {steam_reviews}")
     connection.commit()
-    cursor.close()
-    connection.close()
     print("Game added successfully!")
 
-def update():
-    connection = connect_to_db()
-    cursor = connection.cursor()
+def update(cursor, connection):
     cursor.execute("SELECT steam_id from vr_games")
     steam_ids = cursor.fetchall()
     for (steam_id,) in steam_ids:
         steam_reviews = get_reviews(steam_id)
         cursor.execute("UPDATE vr_games SET steam_reviews = %s WHERE steam_id = %s", (steam_reviews, steam_id));
     connection.commit()
-    cursor.close()
-    connection.close()
     print("Games updated successfully!")
     
 def main():
+
+    connection = connect_to_db()
+    cursor = connection.cursor()
+
     print("\nWelcome to VRGameMeter")
     print("1. List games")
     print("2. Add game")
     print("3. Update games")
     print("4: Get number of Steam reviews for game")
     print("5: Get title of game")
+
     choice = input("Choose an option: ")
 
 ######################### List games ################################
     if choice == '1':
-        list_games()
+        list_games(cursor)
 
 ######################### Add game ##################################
     elif choice == '2':
         steam_id = input("Steam ID: ")
-        vr_only = input("VR Only: ")
-        vr_only = True if vr_only == '1' else False
-        add_game(steam_id, vr_only)
+        vr_only = input("VR Only: ") == '1'
+        add_game(cursor, connection, steam_id, vr_only)
 
 ######################### Update games ##############################
     elif choice == '3':
-        update()
+        update(cursor, connection)
 
 ######################### Get number of reviews #####################
     elif choice == '4':
@@ -98,8 +90,8 @@ def main():
         print(f"The name of the game is {name}.")
 
 ######################### Exit ######################################
-    else:
-        return 0
+    cursor.close()
+    connection.close()
 
 if __name__ == '__main__':
     main()
