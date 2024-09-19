815b22afcbbc15ee2d11e61de375973e1034870b
Julius Friesen <julius.friesen.26@outlook.com>
2024-09-19 21:43:13 +0200

    Known bugs are fixed and basic functionality ensured

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def connect_to_db():
         user='root',
         password='',
         database='vr_games',
-        charset='utf8mb4',  # Setze den Zeichensatz
-        collation='utf8mb4_general_ci'  # Setze die Kollation
+        charset='utf8mb4',
+        collation='utf8mb4_general_ci'
     )
 
 def get_reviews(appid):
@@ -19,54 +19,87 @@ def get_reviews(appid):
     else:
         return None
     
+def get_name(appid):
+    url = f"https://store.steampowered.com/api/appdetails?appids={appid}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()[f'{appid}']['data']['name']
+    else:
+        return None
+    
 def list_games():
     connection = connect_to_db()
     cursor = connection.cursor()
-    cursor.execute("SELECT vr_games.name, vr_games.steam_reviews")
+    cursor.execute("SELECT name, steam_reviews from vr_games order by steam_reviews desc")
     games = cursor.fetchall()
     for game in games:
         print(f"Name: {game[0]}, Steam_Reviews: {game[1]}")
     cursor.close()
     connection.close()
 
-def add_game():
+def add_game(steam_id, vr_only):
     connection = connect_to_db()
     cursor = connection.cursor()
-    sql = "INSERT INTO vr_games (name, steam_id) VALUES (%s, %s)"
-    # TODO Get number of reviews
-    steam_reviews = 0
-    cursor.execute(sql, (name, steam_id, steam_reviews))
+    steam_reviews = get_reviews(steam_id)
+    name = get_name(steam_id)
+    cursor.execute(f"INSERT INTO vr_games (name, steam_id, steam_reviews, vr_only) VALUES (\"{name}\", {steam_id}, {steam_reviews}, {vr_only}) on duplicate key update steam_reviews = {steam_reviews}")
     connection.commit()
     cursor.close()
     connection.close()
     print("Game added successfully!")
+
+def update():
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute("SELECT steam_id from vr_games")
+    steam_ids = cursor.fetchall()
+    for (steam_id,) in steam_ids:
+        steam_reviews = get_reviews(steam_id)
+        cursor.execute("UPDATE vr_games SET steam_reviews = %s WHERE steam_id = %s", (steam_reviews, steam_id));
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print("Games updated successfully!")
     
 def main():
-    while True:
-        print("\nWelcome to VRGameMeter")
-        print("1. List games")
-        print("2. Add game")
-        print("3. Update games")
-        print("4: Get number of Steam reviews for game")
-        print("5: Exit")
-        choice = input("Choose an option: ")
+    print("\nWelcome to VRGameMeter")
+    print("1. List games")
+    print("2. Add game")
+    print("3. Update games")
+    print("4: Get number of Steam reviews for game")
+    print("5: Get title of game")
+    choice = input("Choose an option: ")
 
-        if choice == '1':
-            list_games()
-        elif choice == '2':
-            name = input("Name of game: ")
-            steam_id = input("Steam ID: ")
-            add_game(name, steam_id)
-        elif choice == '3':
-            break #TODO
-        elif choice == '4':
-            app_id = input("App ID of game:")
-            steam_reviews = get_reviews(app_id)
-            print(f"The game has {steam_reviews} total reviews.")
-        elif choice == '5':
-            break
-        else:
-            print("Invalid option. Please try again.")
+######################### List games ################################
+    if choice == '1':
+        list_games()
+
+######################### Add game ##################################
+    elif choice == '2':
+        steam_id = input("Steam ID: ")
+        vr_only = input("VR Only: ")
+        vr_only = True if vr_only == '1' else False
+        add_game(steam_id, vr_only)
+
+######################### Update games ##############################
+    elif choice == '3':
+        update()
+
+######################### Get number of reviews #####################
+    elif choice == '4':
+        steam_id = input("Steam ID:")
+        steam_reviews = get_reviews(steam_id)
+        print(f"The game has {steam_reviews} total reviews.")
+
+######################### Get name of game #########################
+    elif choice == '5':
+        steam_id = input("Steam ID:")
+        name = get_name(steam_id)
+        print(f"The name of the game is {name}.")
+
+######################### Exit ######################################
+    else:
+        return 0
 
 if __name__ == '__main__':
     main()
